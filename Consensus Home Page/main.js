f129344f2b878a5e5fb0ebe7ad5e9ccb616e1cdd
jeffreylind <Jeffrey.lind24@pm.me>
2023-01-06 11:40:25 -0500

    Initial Commit

---
+++ Consensus Home Page/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+
+  const HomePage = new BrowserWindow({
+
+    fullscreen: true,
+    backgroundColor: "#323232"
+
+  });
+
+  HomePage.loadFile('index.html');
+
+};
+
+app.whenReady().then(() => {
+
+  createWindow();
+
+  app.on('activate', () => {
+
+    if (BrowserWindow.getAllWindows().length === 0) {
+
+      createWindow();
+
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
+
+    app.quit();
+    
+  }
+});
