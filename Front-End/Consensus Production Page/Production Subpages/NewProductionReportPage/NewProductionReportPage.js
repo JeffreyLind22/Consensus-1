7ad49cdace03dabae643a0cdc76ed1bdc0bf9c7b
jeffreylind <Jeffrey.lind24@pm.me>
2023-01-18 15:33:41 -0500

    Refactor MySQL Connection Javascript Code

--- Front-End/Consensus Production Page/Production Subpages/NewProductionReportPage/NewProductionReportPage.js
+++ Front-End/Consensus Production Page/Production Subpages/NewProductionReportPage/NewProductionReportPage.js
@@ -1,47 +1,43 @@
 require('../../../Consensus Home Page/node_modules/dotenv').config()
 
-document.getElementById("ConnectToMySQLButton").addEventListener("click", () => {
+// Uses mysql2 npm package to support MySQL caching_sha2_password https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+var MySQL2 = require("../../../Consensus Home Page/node_modules/mysql2"); 
+
+var MySQL2Connection = MySQL2.createConnection({ 
+  host: process.env.ConsensusCoreDataConnectionHost,
+  user: process.env.ConsensusCoreDataConnectionUser,
+  password: process.env.ConsensusCoreDataConnectionPassword,
+  database: process.env.ConsensusCoreDataConnectionDatabase
+});
+
+MySQL2Connection.connect((err) => {
+
+  if(err) {
+
+    return console.log(err.stack);
+
+  }
+
+  console.log("Connection Successful Yayy");
+
+});
+
+MySQL2Connection.query('SELECT * FROM `ConsensusCoreEmployees`', function(err, rows, fields) {
+
+    if(err){
+
+        console.log(`An error ocurred performing the query: \n ${err}`);
+
+        return;
+
+    }
+
+    console.log("Query succesfully executed", rows);
+
+});
+
+MySQL2Connection.end(() => {
+
+  console.log("Connection Closed Woo")
   
-    // Uses mysql2 npm package to support MySQL caching_sha2_password https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
-    var mysql = require("../../../Consensus Home Page/node_modules/mysql2"); 
-
-    var connection = mysql.createConnection({ 
-      host: process.env.ConsensusCoreDataConnectionHost,
-      user: process.env.ConsensusCoreDataConnectionUser,
-      password: process.env.ConsensusCoreDataConnectionPassword,
-      database: process.env.ConsensusCoreDataConnectionDatabase
-    });
-
-    connection.connect((err) => {
-
-      if(err) {
-
-        return console.log(err.stack);
-
-      }
-
-      console.log("Connection Successful Yayy");
-
-    });
- 
-    connection.query('SELECT * FROM `ConsensusCoreEmployees`', function(err, rows, fields) {
-
-        if(err){
-
-            console.log(`An error ocurred performing the query: \n ${err}`);
-
-            return;
-
-        }
-
-        console.log("Query succesfully executed", rows);
-
-    });
- 
-    connection.end(() => {
-
-      console.log("Connection Closed Woo")
-      
-    });
-
-  }, false);
+});
