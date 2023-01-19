78ceda7e2b9a9318986de9f4e43fe5158abfe353
jeffreylind <Jeffrey.lind24@pm.me>
2023-01-19 12:08:24 -0500

    Implement Pool MySQL Connection Method

--- Front-End/Consensus Production Page/Production Subpages/NewProductionReportPage/NewProductionReportPage.js
+++ Front-End/Consensus Production Page/Production Subpages/NewProductionReportPage/NewProductionReportPage.js
@@ -3,41 +3,23 @@ require('../../../Consensus Home Page/node_modules/dotenv').config()
 // Uses mysql2 npm package to support MySQL caching_sha2_password https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
 var MySQL2 = require("../../../Consensus Home Page/node_modules/mysql2"); 
 
-var MySQL2Connection = MySQL2.createConnection({ 
+const ConsensusCoreDataConnection = MySQL2.createPool({
   host: process.env.ConsensusCoreDataConnectionHost,
   user: process.env.ConsensusCoreDataConnectionUser,
   password: process.env.ConsensusCoreDataConnectionPassword,
   database: process.env.ConsensusCoreDataConnectionDatabase
 });
 
-MySQL2Connection.connect((err) => {
+ConsensusCoreDataConnection.query("SELECT * FROM ConsensusCoreEmployees",(err, data) => {
 
   if(err) {
 
-    return console.log(err.stack);
+      console.error(err);
+
+      return;
 
   }
 
-  console.log("Connection Successful Yayy");
+  console.log(data);
 
-});
-
-MySQL2Connection.query('SELECT * FROM `ConsensusCoreEmployees`', function(err, rows, fields) {
-
-    if(err){
-
-        console.log(`An error ocurred performing the query: \n ${err}`);
-
-        return;
-
-    }
-
-    console.log("Query succesfully executed", rows);
-
-});
-
-MySQL2Connection.end(() => {
-
-  console.log("Connection Closed Woo")
-  
 });
